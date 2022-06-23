e181f72667073f47f56dc730810705d846de1f50
arthur-fontaine <0arthur.fontaine@gmail.com>
2022-06-23 13:57:16 +0200

    feat: add a script to randomize a db

---
+++ server/src/utils/fakerDatabase.ts
@@ -0,0 +1,51 @@
+import { faker } from '@faker-js/faker'
+import prisma from './prisma'
+
+const createShop = async () => {
+  await prisma.shop.create({
+    data: {
+      name: faker.company.companyName(),
+      description: faker.company.catchPhrase(),
+      phoneNumber: faker.phone.number(),
+      website: faker.internet.url(),
+      code: { create: {} },
+      location: {
+        create: {
+          name: faker.address.city(),
+          latitude: parseFloat(faker.address.latitude()),
+          longitude: parseFloat(faker.address.longitude()),
+        },
+      },
+    },
+  })
+}
+
+const createUser = async () => {
+  await prisma.user.create({
+    data: {
+      name: faker.name.firstName(),
+      password: faker.internet.password(),
+      birthdate: faker.date.past(),
+      location: {
+        create: {
+          name: faker.address.city(),
+          latitude: parseFloat(faker.address.latitude()),
+          longitude: parseFloat(faker.address.longitude()),
+        },
+      },
+      character: {
+        create: {},
+      },
+      landscape: {
+        create: {},
+      },
+    },
+  })
+}
+
+export const fakerDatabase = async () => {
+  for (let i = 0; i < 100; i++) {
+    await createShop()
+    await createUser()
+  }
+}
