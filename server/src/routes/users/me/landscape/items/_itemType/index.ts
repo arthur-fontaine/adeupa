3f5b3a48d52218dc5c0e3d212c1aace8ca27f594
clement <clementpnc@gmail.com>
2022-06-26 15:51:43 +0200

    feat: add landscape item routes

---
+++ server/src/routes/users/me/landscape/items/_itemType/index.ts
@@ -0,0 +1,141 @@
+import { FastifyPluginAsync } from 'fastify'
+import fastifyPassport from '@fastify/passport'
+import prisma from './../../../../../../utils/prisma'
+import PrismaClient from '@prisma/client'
+
+const itemUnlocked = (item: PrismaClient.Item & {requiredBadges: PrismaClient.Badge[]}, user: PrismaClient.User & {earnedBadges: PrismaClient.Badge[]}): boolean => {
+    for (let index = 0; index < item.requiredBadges.length; index++) {
+      const requiredBadge = item.requiredBadges[index]
+      if (!user.earnedBadges.find((earnedBadge) => earnedBadge.id === requiredBadge.id)) {
+        return false
+      }
+    }
+
+    return true
+}
+
+const landscapeItem: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+
+    fastify.get(
+        '/',
+        { preValidation: fastifyPassport.authenticate('jwt', { authInfo: false }) },
+        async function (request, reply) {
+        if (!request.user) {
+            return reply.unauthorized()
+        }
+
+        const { itemType } = request.params as { itemType: string }
+
+        const user = await prisma.user.findUnique({
+            where: { id: request.user.id },
+            include: { earnedBadges: true }
+        })
+
+        if (!user) {
+            return reply.unauthorized()
+        }
+
+        if (itemType !== 'LandscapeSkyColor' && itemType !== 'LandscapeDistrict') {
+            return reply.badRequest(
+            'itemType should be equal to "LandscapeSkyColor" or "LandscapeDistrict"'
+            )
+        }
+
+        const items = await prisma.item.findMany({
+            where: {
+            ownedBy: { every: { id: user.id } },
+            type: itemType === 'LandscapeSkyColor' ? 'LandscapeSkyColor' : 'LandscapeDistrict',
+            },
+            include: {
+            requiredBadges: true,
+            },
+        })
+
+        const filteredItems = items.map((item) => {
+            return {
+            ...item,
+            unlocked: itemUnlocked(item, user)
+            }
+        })
+
+        return reply.send({ items: filteredItems })
+        }
+    )
+
+    fastify.put(
+        '/',
+        { preValidation: fastifyPassport.authenticate('jwt', { authInfo: false }) },
+        async function (request, reply) {
+        const { itemType } = request.params as { itemType: string }
+        const { itemId } = request.query as { itemId: string }
+
+        if (!request.user) {
+            return reply.unauthorized()
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { id: request.user.id },
+            include: { earnedBadges: true }
+        })
+
+        if (!user) {
+            return reply.unauthorized()
+        }
+
+        if (itemType !== 'LandscapeSkyColor' && itemType !== 'LandscapeDistrict') {
+            return reply.badRequest(
+            'itemType should be equal to "LandscapeSkyColor" or "LandscapeDistrict"'
+            )
+        }
+
+        const character = await prisma.character.findFirst({
+            where: {
+            user: { id: user.id },
+            },
+        })
+
+        if (!character) {
+            return reply.notFound()
+        }
+
+        const item = await prisma.item.findFirst({
+            where: {
+            id: parseInt(itemId),
+            type: itemType === 'LandscapeSkyColor' ? 'LandscapeSkyColor' : 'LandscapeDistrict',
+            },
+            include: { requiredBadges: true }
+        })
+
+        if (!item) {
+            return reply.notFound()
+        }
+
+        if (!itemUnlocked(item, user)) {
+            return reply.unauthorized()
+        }
+
+        const currentSelectedItem = await prisma.item.findFirst({
+            where: {
+            associatedCharacters: { some: { id: character.id } },
+            type: itemType === 'LandscapeSkyColor' ? 'LandscapeSkyColor' : 'LandscapeDistrict',
+            },
+        })
+
+        await prisma.character.update({
+            where: {
+            id: character.id,
+            },
+            data: {
+            enabledItems: {
+                disconnect: { id: currentSelectedItem?.id },
+                connect: [{ id: parseInt(itemId) }],
+            },
+            },
+        })
+
+        return reply.send()
+        }
+    )
+}
+
+export default landscapeItem
