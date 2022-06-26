abaefd7e3a64b0ba8241b3a26af13f295e480b3e
mehdi.chaibi <mehdichaibi05@gmail.com>
2022-06-26 22:17:47 +0200

    feat: add homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 import User from './pages/User/User'
+import Home from './pages/Home/Home'
 
 function App() {
   const isLoggedIn = localStorage.getItem('token') !== null;
@@ -13,7 +14,7 @@ function App() {
     <div style={{ height: "100%" }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={isLoggedIn ? <div/> : <Signup />} />
+          <Route path="/" element={isLoggedIn ? <Home /> : <Signup />} />
 
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
